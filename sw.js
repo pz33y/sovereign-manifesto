cf1470f0a349b6105d97f309a15abcadae642910
SeekKnowledge <may.26.1637.survivor@gmail.com>
2025-11-16 01:11:56 -0500

    Add service worker for offline PWA support

---
+++ sw.js
@@ -0,0 +1,51 @@
+// sw.js
+
+// Service Worker for Offline PWA Support
+
+const CACHE_NAME = 'my-cache-v1';
+const urlsToCache = [
+    '/index.html',
+    '/styles.css',
+    '/app.js',
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Cache hit - return the response from the cached version
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
